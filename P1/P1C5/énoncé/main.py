82be35550d3eb2bddc024a478868e46c3e143602
Hazhar Galeh <hazhar.galeh@ecodev.ch>
2026-01-01 22:59:49 +0100

    Classify data with data types

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+nom = "Hazhar"
+age = 34
+taille = 1.78
+est_etudiant = False
+
+print(nom, "\n", age, "\n", taille, "\n", est_etudiant)
+print(type(nom), "\n", type(age), "\n", type(taille), "\n", type(est_etudiant))
